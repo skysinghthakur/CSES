370f8c58a35af7ea566eecc4325e6391e9506259
skysinghthakur <skysinghthakur@gmail.com>
2022-04-03 05:57:34 +0530

    Wierd Algorithm

---
+++ Introductory Problems/Wierd Algorithm.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    long n;
+    cin>>n;
+    while(n!=1){
+        cout<<n<<" ";
+        if(n&1){
+            n = n*3 + 1;
+        }
+        else{
+            n/=2;
+        }
+    }
+    cout<<n;
+    return 0;
+}
