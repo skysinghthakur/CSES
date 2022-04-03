24e1add97d6c8c98e0babd1da45c4ac96fabfe27
skysinghthakur <skysinghthakur@gmail.com>
2022-04-03 06:27:37 +0530

    Increasing Array

---
+++ Introductory Problems/Increasing Array.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(auto &i: arr){
+        cin>>i;
+    }
+    int prev=arr.front();
+    long long res=0;
+    for(auto &i: arr){
+        if(i<prev)
+            res+=(long long)(prev-i);
+        prev = i;
+    }
+    cout<<res;
+    return 0;
+}
